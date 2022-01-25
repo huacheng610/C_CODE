74e868b007f3bf90f8b141dfe6db93187851099d
huacheng610 <2380618996@qq.com>
2022-01-25 18:20:08 +0800

    c语言

--- 1.25/Project1/Project1/1.25.c
+++ 1.25/Project1/Project1/1.25.c
@@ -1,1 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int age = 100;
+	if (age < 18)
+		printf("未成年\n");
+	else if (age >= 18 && age < 28)
+		printf("青年\n");
+	else if (age >= 28 && age < 50)
+		printf("壮年\n");
+	else if (age >= 50 && age < 90)
+		printf("老年\n");
+	else
+		printf("老不死");
+
+	
+
+
+	return 0;
+}
