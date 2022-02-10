e1febc241c8f18ca2f8b3806044cf3136f8843cc
huacheng610 <2380618996@qq.com>
2022-02-10 18:02:26 +0800

    c语言

---
+++ 2.11/Project1/Project1/test.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	while (left<=right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{                                                            //二分查找法
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			printf("找到了，下标是：%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("找不到\n");
+	}
+	return 0;
+}
+
+
+
+
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 9;
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (k == arr[i])
+//		{
+//			printf("找到了,下标是:%d\n", i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到\n");
+//	return 0;
+//}
+//
+
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret = ret * i;
+//		}
+//		sum = sum + ret;
+//	}
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
