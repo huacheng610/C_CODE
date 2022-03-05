04183892d9baf8d01b3d64627940114216dc871d
huacheng610 <2380618996@qq.com>
2022-03-05 17:10:51 +0800

    c语言

---
+++ 3.4/Project1/Project1/test.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+//第一种方法
+#include<stdio.h>
+int count = 0;
+int Fib(int n)
+{
+	if (n == 3)//测试第三个斐波那契数的计算次数
+	{
+		count++;
+	}
+	if (n <= 2)
+		return 1;
+	else
+		return Fib(n - 1) + Fib(n - 2);
+}
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = Fib(n);
+	printf("ret=%d\n", ret);
+	printf("count=%d\n", count);
+	return 0;
+}
+
+
+////第二种方法
+//#include<stdio.h>
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = Fib(n);
+//	printf("ret=%d\n", ret);
+//	return 0;
+//}
