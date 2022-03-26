eb533423e843a8891c5960850af299a28938fefe
huacheng610 <2380618996@qq.com>
2022-03-26 16:40:15 +0800

    c语言

---
+++ 3.7/Project1/Project1/game.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+
+
+//int main()
+//{
+//	int a, b;
+//	float x, y;
+//	char c1, c2;
+//	scanf("a=%db=%d", &a,&b);
+//	scanf("%f%e", &x, &y);
+//	scanf("%c%c,&c1,&c2");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int c1, c2;
+//	c1 = 197;
+//	c2 = 198;
+//	printf("c1=%c,c2=%c\n", c1, c2);
+//	printf("c1=%d,c2=%d\n", c1, c2);
+//	return 0;
+//}
+
+
+//#include<stdlib.h>
+//#include<stdio.h>
+//void main()
+//{
+//	int a = 1;
+//	int sum = 0;
+//	while (a<= 100)
+//	{
+//		sum = sum + a;
+//		a = a + 1;
+//	}
+//	printf("%d\n", sum);
+//	system("PAUSE");
+//}
---
+++ 3.7/Project2/Project2/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	int f = 0;
+	int sum = a + b + c + d + e + f;
+	scanf("a=%db=%dc=%dd=%de=%df=%d");
+	int k = sum / 6;
+	printf("Æ½¾ùÊý=%d/n", k);
+	return 0;
+}
